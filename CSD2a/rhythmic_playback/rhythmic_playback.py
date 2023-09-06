def77cf532a618da0e0214c1e7ce1683afc17e5d
pierwoudstra <pier.johannes@gmail.com>
2023-09-06 22:17:00 +0200

    added choice of sample to rhythmic playback script

--- CSD2a/rhythmic_playback/rhythmic_playback.py
+++ CSD2a/rhythmic_playback/rhythmic_playback.py
@@ -11,17 +11,26 @@ kick = "samples/kick.wav"
 # ↓
 
 try:
+  sampleChoice = str(input("which sample do u wanna play? \n (type: 'kick' or 'snare') \n"))
+
+  # kiest welke sample in te laden
+  if sampleChoice == str('kick'):
+    wave_object = simpleaudio.WaveObject.from_wave_file(kick)
+  elif sampleChoice == str('snare'):
+    wave_object = simpleaudio.WaveObject.from_wave_file(snare)
+  else:
+    print("please fill in either: kick or snare \n (︶︹︶) ")
+    quit()
+
+  # print(wave_object) <- om te zien of alles goed is ingeladen
+
   numPlaybackTimes = int(input("how often do u want the sample 2 be played? \n"))
 
-  initial = 1
   rhythmArray = []
-  poly_wave_objects = []
 
-
+  initial = 1
   while initial < numPlaybackTimes + 1:
     # dit werkt als de input een int is
-
-    poly_wave_objects.append(simpleaudio.WaveObject.from_wave_file(snare))
 
     # vragen welke ritmes er moeten worden opgeslagen (evenvaak als dat de sample moet klinken)
     rhythmArray.append(float(input("duration nº " + str(initial) + ": \n")))
@@ -39,11 +48,13 @@ try:
 
     # door de wave objects in een array te zetten probeer ik de sample poly te maken
 
-    play_object = poly_wave_objects[initial-1].play()
+    play_object = wave_object.play()
     print(initial)
 
     # berekent tijdsinterval en wacht
     time.sleep(bpmToSeconds * rhythmArray[initial-1])
+
+    play_object.stop()
 
     # play_object.wait_done() <- dit kan ik verwerken in de code zodra ik snap hoe
     # je polyfoon samples af kunt spelen
@@ -51,6 +62,6 @@ try:
     initial += 1
   
 except ValueError:
-  # wat er gebeurt als de input niet het correcte type heeft
 
+  # wat er gebeurt als de input niet het correcte type heeft
   print("please input a number >:(")
