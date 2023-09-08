23af405c92506a0e15d6d62dcc07acae7f504b2d
pierwoudstra <pier.johannes@gmail.com>
2023-09-08 15:37:35 +0200

    added dysfunctional single sample sequencer

---
+++ CSD2a/single_sample_sequencer/single_sample_sequencer.py
@@ -0,0 +1,43 @@
+import simpleaudio as sa
+import time
+
+def getInput():
+    standardBPM = input("the standard BPM is 120, do you want to use this one? y/n \n")
+    if standardBPM == 'y' or standardBPM == 'Y':
+        bpm = 120
+    elif standardBPM == 'n' or standardBPM == 'N':
+        bpm = input("what do you want the BPM to be? ")
+    else:
+        print("please input 'y' or 'n' ")
+    
+    noteArray = [i for i in input("what rhythm pattern do you want to play? ")]
+
+    print(bpm, noteArray)
+
+    return bpm, noteArray
+
+def playRhythm(bpm, noteArray):
+    snare = "samples/snare.wav"
+    wave_object = sa.WaveObject.from_wave_file(snare)
+
+    bpmToSeconds = 60 / bpm
+
+    i = 0
+    while i < 15:
+
+        if i in noteArray:
+            play_object = wave_object.play()
+            time.sleep(bpmToSeconds)
+            play_object.stop()
+        
+        else:
+            time.sleep(bpmToSeconds)
+
+        i += 1
+
+
+def main():
+    bpm, noteArray = getInput()
+    playRhythm(bpm, noteArray)
+
+main()
