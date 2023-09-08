1ccdd9b6f81aa7855b900291aabc7a0d52741e8c
pierwoudstra <pier.johannes@gmail.com>
2023-09-08 17:35:25 +0200

    made functional version of the sample sequencer

--- CSD2a/single_sample_sequencer/single_sample_sequencer.py
+++ CSD2a/single_sample_sequencer/single_sample_sequencer.py
@@ -9,39 +9,78 @@ def getInput():
         bpm = input("what do you want the BPM to be? ")
     else:
         print("please input 'y' or 'n' ")
-    
-    noteArray = [i for i in input("what rhythm pattern do you want to play? ")]
-    # wordt niet overgezet naar ints in een array ↑
+        getInput()
 
-    print(bpm, noteArray)
+    # ↓ array itereert om lijst te maken, bron:  https://devsheet.com/get-integer-only-values-from-a-list-in-python/
+    noteArray = [i for i in input("what rhythm pattern do you want to play? \n write 'k' for kick, 's' for snare and '-' for nothing \n")]
 
     return bpm, noteArray
 
 def playRhythm(bpm, noteArray):
 
-    # maakt nog geen geluid
-
+    # samples inladen
     snare = "samples/snare.wav"
-    wave_object = sa.WaveObject.from_wave_file(snare)
+    kick = "samples/kick.wav"
 
-    bpmToSeconds = 60 / bpm
+    bpmToSeconds = 60 / int(bpm)
 
+    # loopt langs alle tellen en checkt of het een kick of snare is
     i = 0
-    while i < 15:
+    while i < len(noteArray):
 
-        if i in noteArray:
+        print(i)
+
+        # speel kick
+        if noteArray[i] == 'k':
+            wave_object = sa.WaveObject.from_wave_file(kick)
             play_object = wave_object.play()
-            time.sleep(bpmToSeconds)
+            time.sleep(bpmToSeconds/4)
             play_object.stop()
+
+        # speel snare
+        elif noteArray[i] == 's':
+            wave_object = sa.WaveObject.from_wave_file(snare)
+            play_object = wave_object.play()
+            time.sleep(bpmToSeconds/4)
+            play_object.stop()
+
+        # optie om snelle ghost notes te spelen
+        elif noteArray[i].isdigit():
+
+            # hoevaak ghost note zal worden gespeeld
+            amt = noteArray[i]
+
+            if noteArray[i+1] == 'k':
+                for l in range(int(amt)):
+                    wave_object = sa.WaveObject.from_wave_file(kick)
+                    play_object = wave_object.play()
+                    time.sleep((bpmToSeconds/4)/int(amt))
+
+            elif noteArray[i+1] == 's':
+                for l in range(int(amt)):
+                    wave_object = sa.WaveObject.from_wave_file(snare)
+                    play_object = wave_object.play()
+                    time.sleep((bpmToSeconds/4)/int(amt))
+
+            # zorgt ervoor dat lijst toch uitkomt (dat de getallen die achter elkaar moeten als 1 worden gezien)
+            noteArray.pop(i+1)
         
         else:
-            time.sleep(bpmToSeconds)
+            # stilte
+            time.sleep(bpmToSeconds/4)
 
         i += 1
+
+    
+    # playRhythm()
+
+def getCommand():
+    
 
 
 def main():
     bpm, noteArray = getInput()
     playRhythm(bpm, noteArray)
+    getCommand()
 
 main()
