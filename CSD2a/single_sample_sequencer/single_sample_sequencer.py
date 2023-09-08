efbfc331945f44ab4b7dfb50b055946e048100ff
pierwoudstra <pier.johannes@gmail.com>
2023-09-08 18:21:32 +0200

    made sample sequencer loop and tried to implement multithreading but failed

--- CSD2a/single_sample_sequencer/single_sample_sequencer.py
+++ CSD2a/single_sample_sequencer/single_sample_sequencer.py
@@ -1,5 +1,7 @@
 import simpleaudio as sa
 import time
+import threading
+# ↑ multithreading experiment, bron: https://www.geeksforgeeks.org/multithreading-python-set-1/
 
 def getInput():
     standardBPM = input("the standard BPM is 120, do you want to use this one? y/n \n")
@@ -12,7 +14,7 @@ def getInput():
         getInput()
 
     # ↓ array itereert om lijst te maken, bron:  https://devsheet.com/get-integer-only-values-from-a-list-in-python/
-    noteArray = [i for i in input("what rhythm pattern do you want to play? \n write 'k' for kick, 's' for snare and '-' for nothing \n")]
+    noteArray = [i for i in input("what rhythm pattern do you want to play? \n write 'k' for kick, 's' for snare and '-' for nothing \n you can also add numbers in front of the letters to make the sample repeat :o \n ")]
 
     return bpm, noteArray
 
@@ -28,7 +30,7 @@ def playRhythm(bpm, noteArray):
     i = 0
     while i < len(noteArray):
 
-        print(i)
+        # print(i)
 
         # speel kick
         if noteArray[i] == 'k':
@@ -47,23 +49,30 @@ def playRhythm(bpm, noteArray):
         # optie om snelle ghost notes te spelen
         elif noteArray[i].isdigit():
 
-            # hoevaak ghost note zal worden gespeeld
-            amt = noteArray[i]
+            try:
+                # hoevaak ghost note zal worden gespeeld
+                amt = noteArray[i]
 
-            if noteArray[i+1] == 'k':
-                for l in range(int(amt)):
-                    wave_object = sa.WaveObject.from_wave_file(kick)
-                    play_object = wave_object.play()
-                    time.sleep((bpmToSeconds/4)/int(amt))
+                if noteArray[i+1] == 'k':
+                    for l in range(int(amt)):
+                        wave_object = sa.WaveObject.from_wave_file(kick)
+                        play_object = wave_object.play()
+                        time.sleep((bpmToSeconds/4)/int(amt))
 
-            elif noteArray[i+1] == 's':
-                for l in range(int(amt)):
-                    wave_object = sa.WaveObject.from_wave_file(snare)
-                    play_object = wave_object.play()
-                    time.sleep((bpmToSeconds/4)/int(amt))
+                elif noteArray[i+1] == 's':
+                    for l in range(int(amt)):
+                        wave_object = sa.WaveObject.from_wave_file(snare)
+                        play_object = wave_object.play()
+                        time.sleep((bpmToSeconds/4)/int(amt))
+
+                else:
+                    time.sleep(bpmToSeconds/4)
+
+            except IndexError:
+                # index error ontstaat doordat je niet altijd i+1 kunt doen
+                time.sleep(bpmToSeconds/4)
 
             # zorgt ervoor dat lijst toch uitkomt (dat de getallen die achter elkaar moeten als 1 worden gezien)
-            noteArray.pop(i+1)
         
         else:
             # stilte
@@ -71,16 +80,28 @@ def playRhythm(bpm, noteArray):
 
         i += 1
 
-    
-    # playRhythm()
+    playRhythm(bpm, noteArray)
 
 def getCommand():
-    
+    # dit werkt nog niet
+    command = input("type 'stop' to stop the loop, or fill in another rhythm to change the beat (: \n")
 
+    if command == 'stop':
+        quit()
+    else:
+        return command
 
 def main():
     bpm, noteArray = getInput()
-    playRhythm(bpm, noteArray)
-    getCommand()
+    # playRhythm(bpm, noteArray)
+    # getCommand()
+
+    # creating threads
+    t1 = threading.Thread(target=playRhythm, args=(bpm, noteArray))
+    t2 = threading.Thread(target=getCommand, name='t2')
+ 
+    # starting threads
+    t1.start()
+    t2.start()
 
 main()
