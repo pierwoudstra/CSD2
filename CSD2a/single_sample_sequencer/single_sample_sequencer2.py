ccfc8bef4d80fe86de4fe3100d9cee64ecca3d89
pierwoudstra <pier.johannes@gmail.com>
2023-09-09 17:38:32 +0200

    made second version of the sample sequencer with the same functionality but prettier code, i will be keeping the other one for refrence

---
+++ CSD2a/single_sample_sequencer/single_sample_sequencer2.py
@@ -0,0 +1,64 @@
+# versie twee die exact hetzelfde doet als de eerste maar overzichtelijker geordend met behulp van functies
+#
+# ook heb ik alle variablen en functies volgens de python conventie genoemd - bron: https://realpython.com/python-pep8/
+#
+# te doen:
+# multithreading toevoegen of 
+# - om twee ritmes tegelijkertijd te kunnen of 
+# - om commands in te kunnen tijdens dat het programma speelt
+
+import simpleaudio as sa
+import time
+
+def get_bpm():
+
+    # bpm wordt opgehaald
+    # met lower() functie wordt de input als lowercase gelezen
+    standard_bpm = input("the standard BPM is 120, do you want to use this one? y/n \n")
+    return 120 if standard_bpm.lower() == 'y' else int(input("what do you want the BPM to be? "))
+
+def get_rhythm_pattern():
+
+    # ritme patroon wordt opgehaald
+    # ↓ array itereert om lijst te maken, bron:  https://devsheet.com/get-integer-only-values-from-a-list-in-python/
+    return [i for i in input("what rhythm pattern do you want to play? \n write 'k' for kick, 's' for snare and '-' for nothing. \n you can also add numbers in front of the letters to make the sample repeat :o \n")]
+
+def play_sample(file_path, bpm_to_seconds):
+
+    # functie om een sample te spelen
+    wave_object = sa.WaveObject.from_wave_file(file_path)
+    play_object = wave_object.play()
+    time.sleep(bpm_to_seconds/4)
+    play_object.stop()
+
+def play_rhythm_step(note, bpm_to_seconds):
+    if note == 'k':
+        play_sample("samples/kick.wav", bpm_to_seconds)
+    elif note == 's':
+        play_sample("samples/snare.wav", bpm_to_seconds)
+    elif note.isdigit():
+
+        # checkt of er een getal voor letter zit (om rolls te kunnen maken)
+        amt = int(note)
+
+        # hierbij geruik ik een for loop puur voor herhaling
+        # de iterator is niet belangrijk dus kan ik _ neerzetten
+        for _ in range(amt):
+            play_sample("samples/kick.wav", bpm_to_seconds) if note == 'k' else play_sample("samples/snare.wav", bpm_to_seconds)
+            time.sleep((bpm_to_seconds/4)/amt)
+    else:
+        time.sleep(bpm_to_seconds/4)
+
+def play_rhythm(bpm, note_array):
+
+    # loopt door de ingevoerde lijst en roept die functie op de beslist wat daar gespeeld moet worden
+    bpm_to_seconds = 60 / bpm
+    for note in note_array:
+        play_rhythm_step(note, bpm_to_seconds)
+
+def main():
+    bpm = get_bpm()
+    note_array = get_rhythm_pattern()
+    play_rhythm(bpm, note_array)
+
+main()
