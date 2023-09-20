3fec0b24b971bcbfc7c8f18e1770ef8bb372927d
pierwoudstra <pier.johannes@gmail.com>
2023-09-20 23:30:23 +0200

    added dysfunctional event based sequencer

--- CSD2a/assignments/event_based_sequencer.py
+++ CSD2a/assignments/event_based_sequencer.py
@@ -2,43 +2,27 @@ import simpleaudio as sa
 import time
 
 def get_info():
-    rhythm_array = [['k',0], ['h',0], ['s', 0.75], ['s', 1.5], ['k', 2.5], ['s', 3.0], ['s', 4.0]]
-    bpm = 114.0
-    loop_amt = 1
+    rhythm_array = [['kick',0], ['hihat',0], ['snare', 0.75], ['snare', 1.5], ['kick', 2.5], ['snare', 3.0], ['snare', 4.0]]
+    # bpm = 114.0
+    # loop_amt = 1
 
-    return rhythm_array, bpm, loop_amt
+    return rhythm_array
 
 def play_sample(file_path):
     wave_object = sa.WaveObject.from_wave_file(file_path)
-    wave_object.play()
+    play_object = wave_object.play()
+    play_object.wait_done()
 
-def play_array(rhythm_array, bpm, loop_amt):
-
-    # get first timestamp
-    if rhythm_array:
-        current_ts = rhythm_array.pop(0)
-    else:
-        play_array(rhythm_array,bpm,loop_amt)
-
+def play_array(rhythm_array):
     # get starting time
     time_zero = time.time()
 
-    while True:
-        now = time.time() - time_zero
-        if now >= current_ts[1]:
-            play_sample("samples/kick.wav") if current_ts[0] == 'k' else play_sample("samples/snare.wav")
+    current_ts = rhythm_array[0]
 
-            if rhythm_array:
-                current_ts = rhythm_array.pop(0)
-            else:
-                break
-        
-        time.sleep(0.00000001)
-
-
+    play_sample("samples/" + current_ts[0] + ".wav")
 
 def main():
-    rhythm_array, bpm, loop_amt = get_info()
-    play_array(rhythm_array, bpm, loop_amt)
+    rhythm_array = get_info()
+    play_array(rhythm_array)
 
 main()
