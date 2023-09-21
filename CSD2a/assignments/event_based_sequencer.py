e67ff6190596d2bd414486e8e537c38a836ad41a
pierwoudstra <pier.johannes@gmail.com>
2023-09-21 12:08:19 +0200

    added comments

--- CSD2a/assignments/event_based_sequencer.py
+++ CSD2a/assignments/event_based_sequencer.py
@@ -2,27 +2,34 @@ import simpleaudio as sa
 import time
 
 def get_info():
-    rhythm_array = [['kick',0], ['hihat',0], ['snare', 0.75], ['snare', 1.5], ['kick', 2.5], ['snare', 3.0], ['snare', 4.0]]
+    timestamps = [['kick',0], ['hihat',0], ['snare', 0.75], ['snare', 1.5], ['kick', 2.5], ['snare', 3.0], ['snare', 4.0]]
     # bpm = 114.0
     # loop_amt = 1
 
-    return rhythm_array
+    return timestamps
 
 def play_sample(file_path):
     wave_object = sa.WaveObject.from_wave_file(file_path)
     play_object = wave_object.play()
     play_object.wait_done()
 
-def play_array(rhythm_array):
+def play_array(timestamps):
     # get starting time
     time_zero = time.time()
 
-    current_ts = rhythm_array[0]
+    current_ts = timestamps[0]
 
-    play_sample("samples/" + current_ts[0] + ".wav")
+    # TODO make loop that handles timestamps and plays right sample
+    while True:
+
+        if current_ts[1] > time_zero:
+            play_sample("samples/" + current_ts[0] + ".wav")
+        else:
+            break
+
 
 def main():
-    rhythm_array = get_info()
-    play_array(rhythm_array)
+    timestamps = get_info()
+    play_array(timestamps)
 
 main()
