6cc8dcbef88baef81e341f9e05402330e9f05210
pierwoudstra <pier.johannes@gmail.com>
2023-09-26 20:40:46 +0200

    started again with event based sequencer

--- CSD2a/assignments/event_test.py
+++ CSD2a/assignments/event_test.py
@@ -1,10 +1,20 @@
 #
 # Opdracht​:
-# Maak een sequencer die een percussieve loop van minimaal 16 events afspeelt op de momenten die worden aangegeven door hun timestamps.
+# Maak een sequencer die een percussieve loop van minimaal 16 events afspeelt
+# op de momenten die worden aangegeven door hun timestamps.
 # Gebruik drie verschillende samples. Experimenteer met gelijktijdige en sequentiële events.
 # 
-# Maak een functie die één event afhandelt en roep deze aan op het moment dat door de timestamp van de event wordt aangegeven.
+# Maak een functie die één event afhandelt en roep deze aan op het moment
+# dat door de timestamp van de event wordt aangegeven.
 # Zo’n functie wordt een ​event handler​ genoemd.
+#
+
+import simpleaudio as sa
+import time
+
+kick = sa.WaveObject.from_wave_file("samples/kick")
+snare = sa.WaveObject.from_wave_file("samples/snare")
+hihat = sa.WaveObject.from_wave_file("samples/hihat")
 
 rhythm = [
     {timestamp: 0.0, instrument: kick},
