8b7081dd19629d58f9ae468e01f12d3e0dfcc3b5
pier woudstra <pier.johannes@gmail.com>
2023-11-27 19:34:15 +0100

    created guitar object

--- CSD2b/InstrumentInheritance/Main.cpp
+++ CSD2b/InstrumentInheritance/Main.cpp
@@ -3,7 +3,9 @@
 
 int main() {
   StringedInstrument piano("ping", 88, "copper", "hammer");
+  StringedInstrument guitar("tok", 36, "steel", "plectrum");
 
   piano.play(1, 127);
   piano.play(5, 48);
+  guitar.play(1, 28);
 }
