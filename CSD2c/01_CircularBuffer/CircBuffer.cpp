0c155e97c68368a110607bf301a426be95298ac4
pier woudstra <pier.johannes@gmail.com>
2024-02-07 09:49:06 +0100

    added working circular buffer with dynamic allocation

---
+++ CSD2c/01_CircularBuffer/CircBuffer.cpp
@@ -0,0 +1,33 @@
+//
+// Created by pirko on 06/02/2024.
+//
+
+#include "CircBuffer.h"
+
+CircBuffer::CircBuffer(int size) {
+  buffer = new float[size];
+  this->size = size;
+  readHead = 0;
+  writeHead = 0;
+}
+
+CircBuffer::~CircBuffer() {}
+
+void CircBuffer::write(float input) {
+  buffer[writeHead] = input;
+  if (writeHead < size - 1) {
+    writeHead++;
+  } else {
+    writeHead = 0;
+  }
+}
+
+float CircBuffer::read() {
+  float output = buffer[readHead];
+  if (readHead < size - 1) {
+    readHead++;
+  } else {
+    readHead = 0;
+  }
+  return output;
+}
---
+++ CSD2c/01_CircularBuffer/main.cpp
@@ -0,0 +1,27 @@
+#include "CircBuffer.h"
+#include <iostream>
+
+int main() {
+
+  int bufSize = 20;
+
+  CircBuffer circBuffer = CircBuffer(bufSize);
+
+
+  std::cout << "writing values" << std::endl;
+
+  for (int i = 0; i < bufSize; i++) {
+    circBuffer.write(i);
+  }
+
+  std::cout << "reading values" << std::endl;
+
+
+  for (int j = 0; j < 60; j++) {
+    // std::cout << j << std::endl;
+    std::cout << circBuffer.read() << std::endl;
+  }
+
+  std::cout << "done" << std::endl;
+
+}
