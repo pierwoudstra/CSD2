610a416475e02a5945646a97c4788a388ed45d0a
pier woudstra <pier.johannes@gmail.com>
2024-05-08 16:55:31 +0200

    plugin works but has some unexpected sounds

---
+++ CSD2d/p1/BitCrusher.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Effect.h"
+#include <iostream>
+
+class BitCrusher : public Effect {
+public:
+  BitCrusher(double bitDepth = 4.0, float dryWet = 1.0f);
+  ~BitCrusher();
+
+  void setQuantizedBitDepth(double bitDepth);
+
+private:
+  void applyEffect(const float &input, float &output) override;
+
+  double bitDepth;
+  double quantizationLevel;
+
+  BitCrusher() {}
+};
