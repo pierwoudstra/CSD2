a3bcb4b0b4fb1ddb1327050370b3a92336c8b1b5
pier woudstra <pier.johannes@gmail.com>
2024-05-08 17:36:16 +0200

    fixed crackling sound and weird effects

---
+++ CSD2d/build/p1/CMakeFiles/p1.dir/__/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp.o.d
@@ -0,0 +1,12 @@
+p1/CMakeFiles/p1.dir/__/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp.o: \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_core/system/juce_CompilerWarnings.h \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/TargetConditionals.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityMacros.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityVersions.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/Availability.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityInternal.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityInternalLegacy.h \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_audio_processors/utilities/ARA/juce_ARADebug.h \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_core/system/juce_PlatformDefs.h
---
+++ CSD2d/build/p1/CMakeFiles/p1_AU.dir/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_VST3.mm.o.d
@@ -0,0 +1,11 @@
+p1/CMakeFiles/p1_AU.dir/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_VST3.mm.o: \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_VST3.mm \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/TargetConditionals.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityMacros.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityVersions.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/Availability.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityInternal.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.2.sdk/usr/include/AvailabilityInternalLegacy.h \
+  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_core/system/juce_CompilerWarnings.h
