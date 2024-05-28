9c113fadfc519fa73d85a923817fab4c8a16d5c7
pier woudstra <pier.johannes@gmail.com>
2024-05-28 14:45:33 +0200

    finally made xy pad work and remember parameters

--- CSD2d/build/p1/CMakeFiles/p1.dir/Components/background.cpp.o.d
+++
@@ -1,3 +0,0 @@
-p1/CMakeFiles/p1.dir/Components/background.cpp.o: \
-  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/p1/Components/background.cpp \
-  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/p1/Components/background.h
--- CSD2d/build/p1/CMakeFiles/p1_Standalone.dir/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.mm.o.d
+++
@@ -1,3 +0,0 @@
-p1/CMakeFiles/p1_Standalone.dir/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.mm.o: \
-  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.mm \
-  /Users/pirko/Documents/hku/jaar2/csd/CSD2d/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_LV2.cpp
